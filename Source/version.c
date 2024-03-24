c4c0a988abefe6e9dfb094f5202252358c0eb874
Bacchus777 <ablysh@rambler.ru>
2024-03-24 14:41:22 +0300

    Init

---
+++ Source/version.c
@@ -0,0 +1,18 @@
+
+    #ifndef ZCL_APP_VERSION_H
+    #define ZCL_APP_VERSION_H
+
+    #ifdef __cplusplus
+    extern "C" {
+    #endif
+
+    #include "version.h"
+const uint8 zclApp_DateCode[] = { 16, '2', '4', '/', '0', '3', '/', '2', '0', '2', '4', ' ', '1', '4', ':', '3', '5' };
+const char zclApp_DateCodeNT[] = "24/03/2024 14:35";
+
+    #ifdef __cplusplus
+    }
+    #endif
+
+    #endif /* ZCL_APP_VERSION_H */
+    
